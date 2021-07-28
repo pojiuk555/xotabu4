6ac3ffef1350eabf20f4f21f6fb189e1d73fa94d
pojiuk555 <pojiuk555@gmail.com>
2021-07-28 20:24:16 +0300

    temp save

--- test/specs/smoke.ts
+++ test/specs/smoke.ts
@@ -9,5 +9,15 @@ describe('My Sites', () => {
         await browser.url('https://youtube.com/');
         expect(await browser.getTitle()).toEqual('YouTube')
     })
+
+    // it('Check image', async () => {
+    //     await browser.url('https://webdriver.io/');
+    //     let image = $('img[src="https://webdriver.io/img/open-jsf-logo.svg"]')
+    //     // if (!(await image).isExisting) {
+    //     //     throw new Error('no Image on place')
+    //     // }
+        
+
+   // })
 })
 
