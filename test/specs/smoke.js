88ee075da5ca84fa0a3ece316c4d2acc9f5cbe8b
pojiuk555 <pojiuk555@gmail.com>
2021-07-28 05:02:08 +0300

    add jscongig.json and modified smoke test

--- test/specs/smoke.js
+++ test/specs/smoke.js
@@ -1,14 +1,15 @@
-const myPage = require(`https://webdriver.io`);
-const image = $('img[src="https://webdriver.io/img/open-jsf-logo.svg"]');
 
-describe('Website', () => {
-    it('should be on place', async () => {
-        await browser.url('https://webdriver.io');
-       
-        expect().toBeExisting();
-        // if (!image.isExisting()) {
-        //     throw new Error ('There is no images by this link')
-        // }
+
+describe('My Sites', () => {
+    it('Check Titles Lodash', async () => {
+        await browser.url('https://lodash.com/');
+        expect(await browser.getTitle()).toEqual('Lodash')
+    })
+
+    it('Check Titles Youtube', async () => {
+        await browser.url('https://youtube.com/');
+        expect(await browser.getTitle()).toEqual('YouTube')
+    
     })
 })
 
